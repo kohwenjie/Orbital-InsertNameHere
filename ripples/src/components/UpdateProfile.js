69ce6d01b88258785dd5789e0d52aa50728bb638
NIL-99 <mingyong1999@gmail.com>
2021-06-05 18:25:28 +0800

    Functionalities of Ripples Completed
    - Create Account
    - Sign In
    - Sign Out
    - Forgot Password
    - Update Profile (Email and Password only)

--- ripples/src/components/UpdateProfile.js
+++ ripples/src/components/UpdateProfile.js
@@ -3,7 +3,7 @@ import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
 
-export default function Signup() {
+export default function UpdateProfile() {
   const firstNameRef = useRef();
   const lastNameRef = useRef();
   const usernameRef = useRef();
@@ -12,35 +12,45 @@ export default function Signup() {
   const passwordConfirmRef = useRef();
   const contactRef = useRef();
   const dobRef = useRef();
-  const { signup } = useAuth();
+  const { currentUser, updateEmail, updatePassword } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match");
     }
 
-    try {
-      setError("");
-      setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
-    } catch {
-      setError("Failed to create an account");
+    const updates = [];
+    setLoading(true);
+    setError("");
+
+    if (emailRef.current.value !== currentUser.email) {
+      updates.push(updateEmail(emailRef.current.value));
+    }
+    if (passwordRef.current.value) {
+      updates.push(updatePassword(passwordRef.current.value));
     }
 
-    setLoading(false);
+    Promise.all(updates)
+      .then(() => {
+        history.push("/");
+      })
+      .catch(() => {
+        setError("Failed to update account");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
 
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-20">Sign Up</h2>
+          <h2 className="text-center mb-20">Update Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="firstName">
@@ -57,15 +67,28 @@ export default function Signup() {
             </Form.Group>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                defaultValue={currentUser.email}
+              />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                placeholder="Leave blasnk to keep the same"
+              />
             </Form.Group>
             <Form.Group id="confirmpassword">
               <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                placeholder="Leave blank to keep the same"
+              />
             </Form.Group>
             <Form.Group id="contact">
               <Form.Label>Contact</Form.Label>
@@ -76,13 +99,13 @@ export default function Signup() {
               <Form.Control type="dob" ref={dobRef} required />
             </Form.Group>
             <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
+              Update Profile
             </Button>
           </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
+        <Link to="/">Cancel Changes</Link>
       </div>
     </>
   );
