cf54b05cd21fb956046f5577c6d3c0257ebed727
NIL-99 <mingyong1999@gmail.com>
2021-06-21 19:12:37 +0800

    enabled the history portion for volunteers
    volunteers are now able to see the history (both on-going and past completed events) of theirs

--- ripples/src/components/VolunteerPages/VolunteerHistoryPageFolder/HistoryComponent.js
+++ ripples/src/components/VolunteerPages/VolunteerHistoryPageFolder/HistoryComponent.js
@@ -1,33 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
+import { database } from "../../../firebase";
 import { useAuth } from "../../../contexts/AuthContext";
 
-export default function HistoryComponent() {
+export default function SignedUpComponent() {
   const { dbUser } = useAuth();
   const eventArray = dbUser.commitments;
+  const [events, setEvents] = useState([]);
+  const [identity, setIdentity] = useState();
+
+  console.log(dbUser);
+  console.log(dbUser.uid);
+  // event array stalls the eventUIDs only
+  console.log(eventArray);
+
+  const fetchEvents = async () => {
+    let arr = [];
+    database
+      .collection("events")
+      .where("confirmedVolunteers", "array-contains", dbUser.uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          arr.push(doc.data());
+          setIdentity(doc.id);
+        });
+      })
+      .then(setEvents(arr));
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  console.log(events);
+  console.log(eventArray);
+
   return (
     <>
       <div>
-        <h2>HISTORY PORTION</h2>
+        <h2>COMMITMENT PORTION</h2>
       </div>
       <Table striped bordered hover size="sm">
         <thead>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Event Name</th>
+            <th>Event Date</th>
+            <th>Event Location</th>
+            <th>Event Description</th>
           </tr>
         </thead>
         <tbody>
-          {eventArray &&
-            eventArray.map((event) => {
+          {events &&
+            events.map((event) => {
+              const { eventName, eventDescription, eventLocation, eventDate } =
+                event;
               return (
                 <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td>{eventName}</td>
+                  <td>{eventDate}</td>
+                  <td>{eventLocation}</td>
+                  <td>{eventDescription}</td>
                 </tr>
               );
             })}
