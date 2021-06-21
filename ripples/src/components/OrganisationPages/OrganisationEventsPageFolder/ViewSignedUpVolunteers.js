dbcd695369eaca7a9830dc0c6bbae44ef77951a4
kohwenjie <17scwenjie@gmail.com>
2021-06-21 17:12:04 +0800

    update

--- ripples/src/components/OrganisationPages/OrganisationEventsPageFolder/ViewSignedUpVolunteers.js
+++ ripples/src/components/OrganisationPages/OrganisationEventsPageFolder/ViewSignedUpVolunteers.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Modal, Table } from "react-bootstrap";
+import { Button, Modal, Table, Alert } from "react-bootstrap";
 import { database } from "../../../firebase";
 
 export default function ViewSignedUpVolunteers(props) {
@@ -7,7 +7,7 @@ export default function ViewSignedUpVolunteers(props) {
   const [signedUpVolunteersArr, setSignedUpVolunteersArr] = useState([]);
   const [volunteersProfile, setVolunteersProfile] = useState([]);
   const event = props.e;
-  const { eventName, signedUpVolunteers } = event;
+  const { eventName, signedUpVolunteers, confirmedVolunteers } = event;
 
   const fetchVolunteers = async () => {
     let arr = [];
@@ -15,7 +15,7 @@ export default function ViewSignedUpVolunteers(props) {
       let signedUpArr = [];
       signedUpVolunteers.forEach((volUID) => signedUpArr.push(volUID));
       setSignedUpVolunteersArr(signedUpArr);
-      console.log("here 3: ", signedUpVolunteersArr);
+      // console.log("here 3: ", signedUpVolunteersArr);
     }
 
     database
@@ -44,6 +44,39 @@ export default function ViewSignedUpVolunteers(props) {
   function closeModal() {
     setOpen(false);
   }
+
+  function handleAccept(volUID) {
+    if (
+      signedUpVolunteers.includes(volUID) &&
+      !confirmedVolunteers.includes(volUID)
+    ) {
+      let newSignedUpArr = [];
+      signedUpVolunteers.forEach((uid) => {
+        if (uid !== volUID) {
+          newSignedUpArr.push(uid);
+        }
+      });
+      database
+        .collection("events")
+        .doc(event.id)
+        .update({ signedUpVolunteers: newSignedUpArr });
+
+      let newConfirmedArr = [];
+      confirmedVolunteers.forEach((uid) => {
+        newConfirmedArr.push(uid);
+      });
+      newConfirmedArr.push(volUID);
+      database
+        .collection("events")
+        .doc(event.id)
+        .update({ signedUpVolunteers: newConfirmedArr });
+      return <Alert>Volunteer accepted!</Alert>;
+    } else {
+      return <Alert>Error! Cant Accept Volunteer</Alert>;
+    }
+  }
+
+  function handleReject(volUID) {}
 
   return (
     <>
@@ -77,7 +110,9 @@ export default function ViewSignedUpVolunteers(props) {
                       <td>description yet to implement</td>
                       <td>
                         <Button>Reject</Button>
-                        <Button>Accept</Button>
+                        <Button onClick={handleAccept(profile.id)}>
+                          Accept
+                        </Button>
                       </td>
                     </tr>
                   );
