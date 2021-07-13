cf001ce3a98f62866bd45f97de0c3f4e691b31d2
NIL-99 <mingyong1999@gmail.com>
2021-07-13 15:18:23 +0800

    push

--- ripples/src/components/VolunteerPages/VolunteerHomePageFolder/VolunteerHome.js
+++ ripples/src/components/VolunteerPages/VolunteerHomePageFolder/VolunteerHome.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
+import { Card, CardGroup } from "react-bootstrap";
 import Header from "../SharedComponent/Header";
 import MainFeaturedPost from "../SharedComponent/MainFeaturedPost";
 import Footer from "../SharedComponent/Footer";
@@ -16,6 +17,10 @@ const mainFeaturedPost = {
 };
 
 export default function VolunteerHome() {
+  const { dbUser } = useAuth();
+  const { commitments } = dbUser;
+  const numEvents = commitments.length;
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -25,6 +30,20 @@ export default function VolunteerHome() {
           <MainFeaturedPost post={mainFeaturedPost} />
         </main>
       </Container>
+      <CardGroup>
+        <Card>
+          <Card.Body>
+            <Card.Text class="text-center fs-5">
+              Number of Upcoming Events: {numEvents}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <Card.Text class="text-center fs-5">Quote of the Day:</Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup>
       <Footer />
     </React.Fragment>
   );
